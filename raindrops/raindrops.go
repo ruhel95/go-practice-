037871d8541dd80ae315a4a8dc096697c0b056b6
ruhel95 <ruhel.ahmed@nytimes.com>
2018-07-18 15:37:00 -0400

    add factor

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -21,3 +21,18 @@ func Convert(n int) string {
 
 	return store
 }
+
+func factor(x int) []int {
+	fact := make([]int, 0)
+	var save int = 0
+	for i := 1; i <= x; i++ {
+		if x%i == 0 {
+			fact[save] = i
+			save++
+		}
+	}
+	return fact
+}
+
+//prints all the factors of a number
+// stores it in a array
